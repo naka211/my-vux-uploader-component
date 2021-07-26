344acd2106508e2cd94350d273877aa49bbffb47
Trung <nttrung211@yahoo.com>
2021-07-27 02:07:59 +0700

    test 2

--- src/components/uploader.vue
+++ src/components/uploader.vue
@@ -4,7 +4,7 @@
       <p class="vux-uploader_title">{{ title }}</p>
       <div class="vux-uploader_info">{{ fileList.length }} / {{ limit }}</div>
     </div>
-    aaaa
+    <div>test content</div>
     <div class="vux-uploader_bd">
       <ul class="vux-uploader_files">
         <li
